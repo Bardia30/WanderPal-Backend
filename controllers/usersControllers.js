dac6ecf6d0a59ded66d4eb9aecd81b45997b9ec8
Bardia Dehbasti <bardiadehbasti@gmail.com>
2023-09-02 13:54:18 -0400

    added more endpoints

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,9 +1,54 @@
+const User = require('../models/usersModel');
+
+
+// User signup function
 const signup = (req, res) => {
-    //logic to sign user up
+    const { email, password, name, image } = req.body;
+
+    // Check if the email already exists
+    User.findOne({ email })
+        .then(user => {
+            if (user) {
+                return res.status(400).json({ message: 'Email already exists!' });
+            }
+
+            const newUser = new User({
+                email,
+                name,
+                password,
+                image
+            });
+
+            // Save the new user to the database
+            return newUser.save();
+        })
+        .then(result => {
+            res.status(201).json({ message: 'User registered successfully!', userId: result._id });
+        })
+        .catch(err => {
+            res.status(500).send(`Error occurred: ${err.message}`);
+        });
 }
 
+// User login function
 const login = (req, res) => {
-    //logic to log user in. 
+    const { email, password } = req.body;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Authentication failed! No such user found.' });
+            }
+            
+            if (password !== user.password) {
+                return res.status(401).json({ message: 'Authentication failed! Password is incorrect.' });
+            }
+
+            res.status(200).json({ message: 'Login successful!', userId: user._id });
+        })
+        .catch(err => {
+            return res.status(500).send(`Error occurred: ${err.message}`);
+        });
 }
 
 
